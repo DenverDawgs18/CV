5a8c58055840383a9d921612ac0db83921a1a58b
DenverDawgs18 <pautomas55@gmail.com>
2024-03-11 10:15:06 -0400

    The 4 inputs for Education.jsx written

---
+++ src/components/Education.jsx
@@ -0,0 +1,10 @@
+export default function Education(){
+    return (
+        <>
+            <input type="text" placeholder="School name:" />
+            <input type="text" placeholder="Degree (or diploma) obtained: "/>
+            <input type="date" placeholder="Date started: "/>
+            <input type="date" placeholder="Date ended: "/>
+        </>
+    )
+}
