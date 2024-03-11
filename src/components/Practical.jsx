224db7cd2bb4dd7c7ee17a9ec631e1dc04e54818
DenverDawgs18 <pautomas55@gmail.com>
2024-03-11 10:22:21 -0400

    Practical.jsx inputs complete and planning.txt modified

---
+++ src/components/Practical.jsx
@@ -0,0 +1,11 @@
+export default function Practical(){
+    return(
+        <>
+            <input type="text" placeholder="Company name" />
+            <input type="text" placeholder="Position title: " />
+            <input type="text" placeholder="Main responsibilities" />
+            <input type="date" placeholder="Date Started: "/>
+            <input type="date" placeholder="Date Stopped Working There: " />
+        </>
+    )
+}
