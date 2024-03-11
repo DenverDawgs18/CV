701443f946b6c2a49cbedfcf3be5a0fa86ff5fa7
DenverDawgs18 <pautomas55@gmail.com>
2024-03-11 10:24:38 -0400

    CV.jsx created, put other 3 components in it, imported CV into main.jsx

---
+++ src/components/CV.jsx
@@ -0,0 +1,13 @@
+import General from "./General";
+import Education from "./Education";
+import Practical from "./Practical";
+
+export default function CV(){
+    return(
+        <>
+        <General />
+        <Education />
+        <Practical />
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import General from "./components/General.jsx";
+import CV from "./components/CV.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <General />
+    <CV />
   </React.StrictMode>,
 );
