a8cc1dee32561d45ef7c2535d2eb6a636260b2cd
DenverDawgs18 <pautomas55@gmail.com>
2024-03-11 10:09:06 -0400

    Created General.jsx and planned out most of project in planning.txt

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import General from "./components/General.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <General />
   </React.StrictMode>,
 );
