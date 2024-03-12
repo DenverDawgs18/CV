b5270c3e2f80b2f44935d02470044c90a7567cc6
DenverDawgs18 <pautomas55@gmail.com>
2024-03-12 13:01:32 -0400

    General working

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -4,27 +4,49 @@ import Practical from "./Practical";
 import { useState } from "react";
 
 export default function CV(){
-    const [firstName, setFirstName] = useState('')
-    function firstNameChange(e){
-        setFirstName(e.target.value)
+    const [firstName, setFirstName] = useState('');
+    function handleFNChange(e){
+        setFirstName(e.target.value);
     }
+    const [lastName, setLastName] = useState('');
+    function handleLChange(e){
+        setLastName(e.target.value);
+    }
+    const [email, setEmail] = useState('');
+    function handleEChange(e){
+        setEmail(e.target.value);
+    }
+    const [phoneNumber, setPhoneNumber] = useState('');
+    function handlePNChange(e){
+        setPhoneNumber(e.target.value);
+    }
+    
     return(
         <div className="wrapper">
 
         
         
             <form className="main">
-                <General />
+                <General 
+                FNvalue={firstName}
+                handleFChange={handleFNChange}
+                LNvalue={lastName}
+                handleLChange={handleLChange}
+                EValue={email}
+                handleEChange={handleEChange}
+                PNValue={phoneNumber}
+                handlePNChange={handlePNChange}
+                />
                 <Education />
                 <Practical />
             </form>
             <div className="resume">
                 <div className="generalResume">
                     <h3>General</h3>
-                    <p>First name: </p>
-                    <p>Last name: </p>
-                    <p>Email: </p>
-                    <p>Phone number: </p>
+                    <p>First name: {firstName} </p>
+                    <p>Last name: {lastName} </p>
+                    <p>Email: {email} </p>
+                    <p>Phone number: {phoneNumber} </p>
                 </div>
                 <div className="educationResume">
                     <h3>Education</h3>
--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,12 +1,17 @@
-export default function General(){
+export default function General({FNvalue, handleFChange,
+LNvalue, handleLChange, EValue, handleEChange, PNValue, handlePNChange}){
    
    return (
     <div className="general">
         <h1 className="generalH header"> General </h1>
-        <input type="text" placeholder="First name:" />
-        <input type="text" placeholder="Last name: "/>
-        <input type="text" placeholder="Email: " />
-        <input type="tel" placeholder="Phone number: "/>
+        <input type="text" placeholder="First name:" 
+        value={FNvalue} onChange={handleFChange} />
+        <input type="text" placeholder="Last name: "
+        value={LNvalue} onChange={handleLChange}/>
+        <input type="text" placeholder="Email: " 
+        value={EValue} onChange={handleEChange}/>
+        <input type="tel" placeholder="Phone number: "
+        value={PNValue} onChange={handlePNChange}/>
     </div>
    )
   
