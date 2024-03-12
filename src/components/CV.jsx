ca522fb38b83a3f6b4212c8cab0a5399db4315ea
DenverDawgs18 <pautomas55@gmail.com>
2024-03-12 06:12:07 -0400

    Basic resume created

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,10 +1,17 @@
 import General from "./General";
 import Education from "./Education";
 import Practical from "./Practical";
+import { useState } from "react";
 
 export default function CV(){
+    const [firstName, setFirstName] = useState('')
+    function firstNameChange(e){
+        setFirstName(e.target.value)
+    }
     return(
-        <>
+        <div className="wrapper">
+
+        
         
             <form className="main">
                 <General />
@@ -12,8 +19,32 @@ export default function CV(){
                 <Practical />
             </form>
             <div className="resume">
+                <div className="generalResume">
+                    <h3>General</h3>
+                    <p>First name: </p>
+                    <p>Last name: </p>
+                    <p>Email: </p>
+                    <p>Phone number: </p>
+                </div>
+                <div className="educationResume">
+                    <h3>Education</h3>
+                    <p>School name: </p>
+                    <p>Degree obtained: </p>
+                    <p>From XXX to XXX</p>
                 
+                {/* Will probably need to factor this and practicalResume into own components to introduce
+                the idea of multiple degrees or jobs */}
+                 
+                </div>
+                <div className="practicalResume">
+                    <h3>Practical Experience</h3>
+                    <p>Company name: </p>
+                    <p>Position title: </p>
+                    <p>Main responsibilities: </p>
+                    <p>From XXX to XXX</p>
+                </div>
+
             </div>
-        </>
+        </div>
     )
 }
--- src/components/Practical.jsx
+++ src/components/Practical.jsx
@@ -1,7 +1,7 @@
 export default function Practical(){
     return(
         <div className="practical">
-            <h1 className="practicalH header">Pracitcal Experience</h1>
+            <h1 className="practicalH header">Practical Experience</h1>
             <input type="text" placeholder="Company name" />
             <input type="text" placeholder="Position title: " />
             <input type="text" placeholder="Main responsibilities" />
